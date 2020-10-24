1829ab3c10e42e1280198e199d4f66d2d0993462
dspv <dmitriy@solodukha.com>
2020-10-24 16:50:33 +0300

    add 10-step web-server laucnh

---
+++ archive/_main-1.tf
@@ -0,0 +1,83 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# 1. VPC
+# 2. IGW
+# 3. Custom Route Table
+# 4. Subnet
+# 5. Associate subnet the route table
+# 6. Secutury Group to allow ports 22, 80, 443
+# 7. Network interface with an IP in the subnet that was created on step 4
+# 8. Assign an EIP to the network interface create on step 7
+# 9. Install Ubuntu server and install/ebable Apache 2
+# test
+
+
+
+#resource "aws_instance" "terra-nova" {
+#  ami               = "ami-0947d2ba12ee1ff75"
+#  instance_type     = "t2.micro"
+#  availability_zone = "us-east-1a"
+#  key_name          = "dspv1-kp"
+#  volume_size       = "30"
+#
+#  tags = {
+#    Name = "Terra Nova"
+#  }
+#}
+
+### VPC ###
+############
+resource "aws_vpc" "terra-main-vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "Terra Main VPC"
+  }
+}
+
+resource "aws_vpc" "terra-second-vpc" {
+  cidr_block = "10.1.0.0/16"
+
+  tags = {
+    Name = "Terra Second VPC"
+  }
+}
+
+### Subnets ###
+###############
+resource "aws_subnet" "terra-subnet-1" {
+  vpc_id     = aws_vpc.terra-main-vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "production"
+  }
+}
+
+resource "aws_subnet" "terra-subnet-2" {
+  vpc_id     = aws_vpc.terra-second-vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "development"
+  }
+}
+
+
+# resource "<provider>_<resource-type>" "name" {
+#   Config option 
+#   key = "value"
+#   key2 = "another value"
+# }
--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
+########################
+### Some experiments ###
+########################
 terraform {
   required_providers {
     aws = {
@@ -7,75 +10,155 @@ terraform {
   }
 }
 
-# 1. VPC
-# 2. IGW
-# 3. Custom Route Table
-# 4. Subnet
-# 5. Associate subnet the route table
-# 6. Secutury Group to allow ports 22, 80, 443
-# 7. Network interface with an IP in the subnet that was created on step 4
-# 8. Assign an EIP to the network interface create on step 7
-# 9. Install Ubuntu server and install/ebable Apache 2
-# test
-
-# Configure the AWS Provider
+### Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
 }
 
-#resource "aws_instance" "terra-nova" {
-#  ami               = "ami-0947d2ba12ee1ff75"
-#  instance_type     = "t2.micro"
-#  availability_zone = "us-east-1a"
-#  key_name          = "dspv1-kp"
-#  volume_size       = "30"
-#
-#  tags = {
-#    Name = "Terra Nova"
-#  }
-#}
-
-### VPC ###
-############
-resource "aws_vpc" "terra-main-vpc" {
+### 1. VPC
+resource "aws_vpc" "prod-vpc" {
   cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "Terra Main VPC"
-  }
-}
-
-resource "aws_vpc" "terra-second-vpc" {
-  cidr_block = "10.1.0.0/16"
-
-  tags = {
-    Name = "Terra Second VPC"
-  }
-}
-
-### Subnets ###
-###############
-resource "aws_subnet" "terra-subnet-1" {
-  vpc_id     = aws_vpc.terra-main-vpc.id
-  cidr_block = "10.0.1.0/24"
 
   tags = {
     Name = "production"
   }
 }
 
-resource "aws_subnet" "terra-subnet-2" {
-  vpc_id     = aws_vpc.terra-second-vpc.id
-  cidr_block = "10.0.1.0/24"
+### 2. IGW
+resource "aws_internet_gateway" "prod-igw" {
+  vpc_id = aws_vpc.prod-vpc.id
 
   tags = {
-    Name = "development"
+    Name = "prod-igw"
   }
 }
 
+### 3. Route Table
+resource "aws_route_table" "prod-route-table" {
+  vpc_id = aws_vpc.prod-vpc.id
 
-# resource "<provider>_<resource-type>" "name" {
-#   Config option 
-#   key = "value"
-#   key2 = "another value"
-# }
+  route {
+    cidr_block = "0.0.0.0/0" # All traff
+    gateway_id = aws_internet_gateway.prod-igw.id
+  }
+
+  route {
+    ipv6_cidr_block        = "::/0"
+    gateway_id = aws_internet_gateway.prod-igw.id
+  }
+
+  tags = {
+    Name = "prod-route-table"
+  }
+}
+
+### 4. Subnet
+resource "aws_subnet" "prod-subnet-1" {
+  vpc_id            = aws_vpc.prod-vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "prod-subnet"
+  }
+}
+
+### 5. Associate subnet the route table
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.prod-subnet-1.id
+  route_table_id = aws_route_table.prod-route-table.id
+}
+
+### 6. Secutury Group to allow ports 22, 80, 443
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web_traffic"
+  description = "Allow WEB traffic"
+  vpc_id      = aws_vpc.prod-vpc.id
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }  
+
+ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow-web"
+  }
+}
+
+### 7. Network interface with an IP in the subnet that was created on step 4
+resource "aws_network_interface" "web-server-nic" {
+  subnet_id       = aws_subnet.prod-subnet-1.id
+  private_ips     = ["10.0.1.50"]
+  security_groups = [aws_security_group.allow_web.id]
+
+#  attachment {
+#    instance     = aws_instance.test.id
+#    device_index = 1
+#  }
+}
+
+### 8. Assign an EIP to the network interface create on step 7
+resource "aws_eip" "one" {
+  vpc                       = true
+  network_interface         = aws_network_interface.web-server-nic.id
+  associate_with_private_ip = "10.0.1.50"
+  depends_on                = [aws_internet_gateway.prod-igw]
+}
+
+# 9. Install Ubuntu server and install/ebable Apache 2
+resource "aws_instance" "web-server-instance" {
+  ami               = "ami-0947d2ba12ee1ff75"
+  instance_type     = "t2.micro"
+  availability_zone = "us-east-1a"
+  key_name          = "dspv1"
+  
+  root_block_device {
+    volume_size       = "30"
+  }
+  
+  network_interface {
+    device_index         = 0
+    network_interface_id = aws_network_interface.web-server-nic.id
+
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd.x86_64
+              systemctl start httpd.service
+              systemctl enable httpd.service
+              echo "<h1>Hello World from $(hostname -f)</h1><br><h2>I've been made by Terraform</h2>" > /var/www/html/index.html
+              EOF
+
+  tags = {
+    Name = "prod-web-server"
+  }
+}
+
